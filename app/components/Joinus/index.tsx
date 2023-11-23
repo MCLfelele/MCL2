1b9a7f2bc66f70e3dc933ce5f2a53a604c006f38
MCLfelele <149048326+MCLfelele@users.noreply.github.com>
2023-11-23 21:16:15 +0000

    Add files via upload

--- app/components/Joinus/index.tsx
+++ app/components/Joinus/index.tsx
@@ -1,13 +1,13 @@
 
 const Join = () => {
     return (
-        <div className="bg-joinus my-32">
+        <div className="bg-joinus my-32" id="enroll">
             <div className='mx-auto max-w-2xl lg:max-w-7xl sm:py-4 lg:px-8'>
 
                 <div className="text-center">
-                    <h3 className="text-blue text-lg font-normal tracking-widest">JOIN US</h3>
-                    <h2 className="text-4xl sm:text-6xl font-bold my-6 leading-10"> Take your business to <br /> the new level.</h2>
-                    <p className="text-lightblack text-base font-normal">Craven omni memoria patriae zombieland clairvius narcisse religionis sunt diri undead <br /> historiarum. Golums, zombies unrelenting et Raimi fascinati beheading.</p>
+                    <h3 className="text-blue text-lg font-normal tracking-widest">Enroll</h3>
+                    <h2 className="text-4xl sm:text-6xl font-bold my-6 leading-10"> Join us today as <br /> as you enroll your kids.</h2>
+                    <p className="text-lightblack text-base font-normal">We accept kids on all classes from todlers, preschool, elementary and secondary. Leave your details with us and we'll be in touch</p>
                 </div>
 
                 <div className="mx-auto max-w-4xl pt-5">
@@ -16,11 +16,11 @@ const Join = () => {
                             <input type="name" className="my-4 py-4 sm:pl-6 lg:text-xl text-black sm:rounded-full bg-lightgrey pl-1 focus:outline-none bg-emailbg focus:text-black" placeholder="Your name" autoComplete="off" />
                         </div>
                         <div>
-                            <input type="email" className="my-4 py-4 sm:pl-6 lg:text-xl text-black sm:border-l border-linegrey bg-lightgrey focus:outline-none bg-emailbg focus:text-black" placeholder="Your email" autoComplete="off" />
+                            <input type="number" className="my-4 py-4 sm:pl-6 lg:text-xl text-black sm:border-l border-linegrey bg-lightgrey focus:outline-none bg-emailbg focus:text-black" placeholder="Phone no." autoComplete="off" />
                         </div>
                         <div className="sm:mr-3">
                             <button type="submit" className="joinButton w-full sm:w-0 text-xl text-white font-semibold text-center rounded-xl sm:rounded-full bg-blue hover:bg-btnblue">
-                                Join!
+                                Enroll!
                             </button>
                         </div>
                     </div>
